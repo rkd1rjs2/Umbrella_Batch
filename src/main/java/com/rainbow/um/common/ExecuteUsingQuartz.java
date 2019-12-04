87a0e50c55ea7f670074711635cf96533f6ac35e
kim93 <kim93@DESKTOP-R9EAQ1O>
2019-12-04 14:40:38 +0900

    추천도서 등록 완료

--- src/main/java/com/rainbow/um/common/ExecuteUsingQuartz.java
+++ src/main/java/com/rainbow/um/common/ExecuteUsingQuartz.java
@@ -1,5 +1,7 @@
 package com.rainbow.um.common;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.mail.internet.MimeMessage;
@@ -8,7 +10,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
+import com.rainbow.um.dto.BoardDto;
 import com.rainbow.um.dto.SmsData;
+import com.rainbow.um.model.IBoardService;
 import com.rainbow.um.model.IBookSMSService;
 
 
@@ -19,6 +23,9 @@ public class ExecuteUsingQuartz {
 	
 	@Autowired
 	private IBookSMSService service;
+	
+	@Autowired
+	private IBoardService boardService;
 	
 	
 	public void sendMessage() throws Exception {
@@ -33,6 +40,14 @@ public class ExecuteUsingQuartz {
 	}
 	
 	public void bobListInsert() {
+		BoardDto dto = new BoardDto();
+		Calendar cal = Calendar.getInstance();
+		cal.add(cal.MONTH,-1);
+		SimpleDateFormat dateFormat = new SimpleDateFormat("YYYYMM");
+		String beforeMonth = dateFormat.format(cal.getTime()).substring(4,6);
+		String beforeYear = dateFormat.format(cal.getTime()).substring(0,4);
+		dto.setBoard_title(beforeYear+"년 "+beforeMonth+"월 추천 도서!");
+		boardService.bobInsert(dto);
 		
 	}
 	
