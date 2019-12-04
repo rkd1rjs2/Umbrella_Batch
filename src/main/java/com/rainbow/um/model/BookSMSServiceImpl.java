7346c262337e653ce4b3762063a8ad3426ec0712
ASUS <ASUS@DESKTOP-44OB82G>
2019-12-04 16:04:50 +0900

    매 달, 매일 날짜 설정

--- src/main/java/com/rainbow/um/model/BookSMSServiceImpl.java
+++ src/main/java/com/rainbow/um/model/BookSMSServiceImpl.java
@@ -35,20 +35,21 @@ public class BookSMSServiceImpl  implements IBookSMSService{
 		if(bookList.size()>0?true:false) {
 			for(String book_cseq : bookList) {
 				UserForSms forSms = dao.chkUser(book_cseq);
-				HashMap<String, String> message = new HashMap<String, String>();
-				String bookName = forSms.getBook_name();
-				if(bookName.length()>8) {
-					bookName = bookName.substring(0, 6) + "..";
+				if(forSms != null) {
+					HashMap<String, String> message = new HashMap<String, String>();
+					String bookName = forSms.getBook_name();
+					if(bookName.length()>8) {
+						bookName = bookName.substring(0, 6) + "..";
+					}
+					message.put("to", forSms.getUser_phone());
+					message.put("text", "회원님께서 예약하신 도서 " + bookName + "가 현재 대출 가능한 상태입니다.");
+					try {
+						//sms.send(message);
+					} catch (Exception e) {
+						System.out.println("메세지 전송 에러");
+					}
+					dao.updateStep(book_cseq);
 				}
-				message.put("to", forSms.getUser_phone());
-				message.put("text", "회원님께서 예약하신 도서 " + bookName + "가 현재 대출 가능한 상태입니다.");
-				try {
-					sms.send(message);
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					System.out.println("메세지 전송 에러");
-				}
-				dao.updateStep(book_cseq);
 			}
 		}
 		
