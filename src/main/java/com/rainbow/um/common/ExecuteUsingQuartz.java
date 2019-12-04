5482d036381e41e844fb5b719d3b3c2330f81420
ASUS <ASUS@DESKTOP-44OB82G>
2019-12-04 14:46:16 +0900

    <>

--- src/main/java/com/rainbow/um/common/ExecuteUsingQuartz.java
+++ src/main/java/com/rainbow/um/common/ExecuteUsingQuartz.java
@@ -1,5 +1,8 @@
 package com.rainbow.um.common;
 
+import java.util.HashMap;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 import javax.mail.internet.MimeMessage;
@@ -8,7 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 
+import com.rainbow.um.dto.BoardDto;
 import com.rainbow.um.dto.SmsData;
+import com.rainbow.um.model.IBoardService;
 import com.rainbow.um.model.IBookSMSService;
 
 
@@ -20,12 +25,28 @@ public class ExecuteUsingQuartz {
 	@Autowired
 	private IBookSMSService service;
 	
+	@Autowired
+	private SMSauth sms;
+	@Autowired
+	private IBoardService boardService;
+	
 	
 	public void sendMessage() throws Exception {
-		List<SmsData> liste= service.getSmsData();
-		for (int i = 0; i < liste.size(); i++) {
-			System.out.println(liste.get(i));
+		List<SmsData> lists= service.getSmsData();
+		String to = "phone";
+		String text = "내용";
+		
+		HashMap<String, String> message = new HashMap<String, String>();
+			for (int i = 0; i < lists.size(); i++) {
+				to=lists.get(i).getUser_phone();
+				text="[반납알림문자] 대출하신 "+lists.get(i).getBook_name()+"의 반납일이 "+lists.get(i).getMdate()+"일 남았습니다"
+						+ "연체에대한 불이익이 발생할수있으니 반납일 전까지 반납 부탁드립니다.";
+				message.put("to", to);
+				message.put("text", text);
+				sms.send(message);
 		}
+		
+		
 	}
 	
 	public void resvControll() {
@@ -33,6 +54,14 @@ public class ExecuteUsingQuartz {
 	}
 	
 	public void bobListInsert() {
+		BoardDto dto = new BoardDto();
+		Calendar cal = Calendar.getInstance();
+		cal.add(cal.MONTH,-1);
+		SimpleDateFormat dateFormat = new SimpleDateFormat("YYYYMM");
+		String beforeMonth = dateFormat.format(cal.getTime()).substring(4,6);
+		String beforeYear = dateFormat.format(cal.getTime()).substring(0,4);
+		dto.setBoard_title(beforeYear+"년 "+beforeMonth+"월 추천 도서!");
+		boardService.bobInsert(dto);
 		
 	}
 	
